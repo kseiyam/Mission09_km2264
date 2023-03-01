7efa100b75ec8aa412b0005121f2bbf7631c1220
Kitt Mori <km2264@byu.edu>
2023-02-28 21:52:27 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_km2264.Models;
+using Mission09_km2264.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_km2264.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1)
+        {
+            //results per page
+            int numResults = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip(numResults * (pageNum - 1))
+                .Take(numResults),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = numResults,
+                    CurrentPage = pageNum
+                }
+            };
+            return View(x);
+        }
+    }
+}
---
+++ Models/ViewModels/PageInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_km2264.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumBooks { get; set; }
+        public int BooksPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        //return number of pages required rounded up
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumBooks / BooksPerPage);
+    }
+}
